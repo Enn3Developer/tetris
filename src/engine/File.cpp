0f151c93533f3e13fd857701c2e9c4672123141e
ZhouFede0128 <zhoufederico0128@gmail.com>
2024-10-11 18:04:22 +0200

    created file.h and file.cpp

--- src/engine/File.cpp
+++ src/engine/File.cpp
@@ -1,5 +1,31 @@
-//
-// Created by WIN on 2024/7/10.
-//
-
 #include "File.h"
+
+File::File(char name, t){
+	strcpy(file_name, name);
+	strcpy(text, t);
+}
+
+char* File:: read_file(){
+	ifstream inputFile;
+	char name[20];
+	strcpy(file_name, name);
+	strcat(name, ".txt");
+	inputFile.open(name);
+	while(!inputFile.eof()){
+		inputFile.get(text);
+	}
+	inputFile.close();
+	return text;
+}
+
+void write_file(){
+	ofstream outputFile;
+	char name[20];
+	strcpy(file_name, name);
+	strcat(name, ".txt");
+	outputFile.open(name);
+	outputFile<<text;
+	outputFile<<"\n";
+	outputFile.close();
+	return;
+}
--- src/engine/File.h
+++ src/engine/File.h
@@ -2,9 +2,20 @@
 #define FILE_H
 #include <iostream>
 #include <cstring>
+#include <fstream>
+
+using namespace std;
 
 class File{
+protected:
+    char file_name[20];
+    char text[200];
+public:
+    File (char name, char t);
+    char* read_file();
+    void write_file();
 
+    }
 }
 
 #endif //FILE_H
